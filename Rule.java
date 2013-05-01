a9660b0f2cdff2062599b5c2ca6f6e832f2c10f7
MarkSA2 <puc.mark@gmail.com>
2013-05-01 16:07:23 -0700

    Added method "TODO:" stubs. Added Class and method descriptions.

--- Rule.java
+++ Rule.java
@@ -1,10 +1,103 @@
+
 /**
+ * Based on specified scenarios, a Rule tells the Strategy how it should make moves on the Tic-Tac-Toe Board. 
+ * 
  * @author Mark Adams
- * @version 2013/04/30
+ * @version	2013/05/01
  */
-public class Rule extends Board 
-{	
+public class Rule 
+{
+	private static final long serialVersionUID = 1L;
+	
 	public Rule() 
 	{
+
+	}
+	
+	/**
+	 * Rotate a Rule 90 degrees clockwise.
+	 * 
+	 * @return A Rule that has been rotated 90 degrees clockwise.
+	 */
+	public Rule rotate90DegCW()
+	{
+		//TODO: Create "rotate90DegCW" method.
+		return new Rule();
+	}
+	
+	/**
+	 * Determines the column or row number based on the given starting position.
+	 * 
+	 * @param position1 -> The first position in the set of three positions.
+	 * @return An integer representing the column or row number that is started by the first position.
+	 */
+	public int rowOrColNum(int position1)
+	{
+		//TODO: Create "rowOrColNum" method.
+		return 0;
+	}
+	
+	/**
+	 * Calculates the 90 degrees counter-clockwise rotated middle position of a board containing only three positions.
+	 * Assumes that together the positions make up a distinct row or column.
+	 * 
+	 * @param position1 -> The first position in the set of three positions.
+	 * @param position3 -> The last position in the set of three positions.
+	 * @return An integer representing the rotated middle position.
+	 */
+	public int rotate90DegCCMiddlePos(int position1, int position3)
+	{
+		//TODO: Create "rotate90DegCCMiddlePos" method.
+		return 0;
+	}
+	
+	/**
+	 * Calculates the 90 degrees counter-clockwise rotated positions for a board containing only three positions.
+	 * Assumes that together the positions make up a distinct row or column.
+	 * 
+	 * @param position1 -> The first position in the set of three positions.
+	 * @param position2 -> The middle position in the set of three positions.
+	 * @param position3 -> The last position in the set of three positions.
+	 * @return An integer array containing the rotated positions.
+	 */
+	public int[] rotated90DegCCSection(int position1, int position2, int position3)
+	{
+		//TODO: Create "rotated90DegCCSection" method. 
+		return new int[0];
+	}
+	
+	/**
+	 * Shift the Rule contents one space horizontally (downward). 
+	 * The bottom-most row of the Rule will place its contents in the positions of the top-most row.
+	 * 
+	 * @return A Rule that has had its contents shifted one space horizontally.
+	 */
+	public Rule shiftHorizontal()
+	{
+		//TODO: Create "shiftHorizontal" method.
+		return new Rule();
+	}
+	
+	/**
+	 * Shift the Rule contents one space vertically (right). 
+	 * The right-most column of the Rule will place its contents in the positions of the left-most columns.
+	 * 
+	 * @return A Rule that has had its contents shifted one space vertically.
+	 */
+	public Rule shiftVertical()
+	{
+		//TODO: Create "shiftVertical" method.
+		return new Rule();
+	}
+	
+	/**
+	 * Flip the Rule contents. The contents of the left and right columns will be switched.
+	 * 
+	 * @return A board which has switched its left column contents with its right column contents. 
+	 */
+	public Rule flip()
+	{
+		//TODO: Create "flip" method.
+		return new Rule();
 	}
 }
