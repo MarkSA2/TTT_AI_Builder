9dde3b26cdc67e584560fbf4780a8898ce1b110d
MarkSA2 <puc.mark@gmail.com>
2013-05-01 16:09:29 -0700

    Added method "TODO:" stubs. Added Class and method descriptions.

--- Strategy.java
+++ Strategy.java
@@ -1,10 +1,33 @@
+
 /**
+ * Uses Rules to determine which move to make next on the Tic-Tac-Toe Board.
+ * 
  * @author Mark Adams
- * @version 2013/04/30
+ * @version 2013/05/01
  */
 public class Strategy 
 {
-  public Strategy()
-  {
-  }
+  private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Adds a given Rule to the collection of Rules.
+	 * 
+	 * @param The Rule to add to the collection of Rules.
+	 */
+	public void addRule(Rule rule)
+	{
+		//TODO: Create "addRule" method.
+	}
+	
+	/**
+	 * Searches the collection of Rules for a Rule that applies to the given Tic-Tac-Toe Board.
+	 * 
+	 * @param board -> A Board to compare which the collection of Rules.
+	 * @return The first Rule found that applies to the given Board.
+	 */
+	public Rule findMatchingRule(Board board)
+	{	
+		//TODO: Create "findMatchingRule" method.
+		return new Rule();
+	}
 }
