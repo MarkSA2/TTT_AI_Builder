b90d02dd235bd91f701cd0ef5803d05a80197e5d
MarkSA2 <puc.mark@gmail.com>
2013-05-01 17:38:05 -0700

    Update BoardTest.java
    Added getPos tests.
    Added another test for toString.
    Changed the setPos tests to use .equals() method rather than the == comparison operator.

--- BoardTest.java
+++ BoardTest.java
@@ -4,15 +4,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Test the Board Class methods. 
- *
+ * 
  * @author Mark Adams
  * @version 2013/05/01
  */
 public class BoardTest 
 {
-  Board board;
-  
+	Board board;
+	
 	@Before
 	public void setUp() throws Exception
 	{
@@ -30,7 +29,8 @@ public class BoardTest
 		assertEquals(true, this.board.get(1).get(2).equals(' '));
 		assertEquals(true, this.board.get(2).get(0).equals(' '));
 		assertEquals(true, this.board.get(2).get(1).equals(' '));
-		assertEquals(true, this.board.get(2).get(2).equals(' '));		
+		assertEquals(true, this.board.get(2).get(2).equals(' '));	
+		assertEquals(false, this.board.get(1).get(2).equals('X'));
 	}
 	
 	@Test
@@ -39,21 +39,37 @@ public class BoardTest
 		assertEquals(true, this.board.toString().equals("   \n   \n   \n"));
 		assertEquals(false, this.board.toString().equals("   \n   \n   "));
 		assertEquals(false, this.board.toString().equals("         "));
+		
+		this.board.setPos(0, 1, 'X');
+		assertEquals(true, this.board.toString().equals(" X \n   \n   \n"));
+	
 	}
 	
 	@Test
 	public void testSetPos()
 	{
 		this.board.setPos(0, 1, 'X');
-		assertEquals(true, this.board.get(0).get(1) == 'X');
-		assertEquals(false, this.board.get(0).get(1) == ' ');
+		assertEquals(true, this.board.get(0).get(1).equals('X'));
+		assertEquals(false, this.board.get(0).get(1).equals(' '));
 		
 		this.board.setPos(2, 2, '?');
-		assertEquals(true, this.board.get(2).get(2) == '?');
-		assertEquals(false, this.board.get(2).get(2) == '0');
+		assertEquals(true, this.board.get(2).get(2).equals('?'));
+		assertEquals(false, this.board.get(2).get(2).equals('0'));
 		
 		this.board.setPos(1, 1, ' ');
-		assertEquals(true, this.board.get(1).get(1) == ' ');
-		assertEquals(false, this.board.get(1).get(1) == '?');
+		assertEquals(true, this.board.get(1).get(1).equals(' '));
+		assertEquals(false, this.board.get(1).get(1).equals('?'));
+	}
+	
+	@Test
+	public void testGetPos()
+	{
+		assertEquals(true, this.board.getPos(0, 0) == ' ');
+		assertEquals(true, this.board.getPos(1, 1) == ' ');
+		assertEquals(true, this.board.getPos(2,2) == ' ');
+		
+		this.board.setPos(1, 0, 'O');
+		assertEquals(false, this.board.getPos(1, 0) == ' ');
+		assertEquals(true, this.board.getPos(1, 0) == 'O');
 	}
 }
